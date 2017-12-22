2556a73e2f0f5e7936265d539ae9993a06b78dd1
zms00@139.com <zms00@139.com>
2017-12-22 20:37:25 +0800

    ???

--- app/src/main/java/com/finance/abby/abby/ListActivity.java
+++ app/src/main/java/com/finance/abby/abby/ListActivity.java
@@ -3,15 +3,20 @@ package com.finance.abby.abby;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.finance.abby.abby.AddActivity.DataBaseName;
+
 public class ListActivity extends AppCompatActivity {
+    private ListView listView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,3 +46,4 @@ public class ListActivity extends AppCompatActivity {
         }
     }
 }
+
